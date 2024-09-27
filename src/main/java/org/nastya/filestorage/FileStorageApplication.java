effa073bff73d150789a259ddecc9fd6227c3b5d
Nasstyaaa <Luk-2004@mail.ru>
2024-09-27 15:01:08 +0300

    init commit

---
+++ src/main/java/org/nastya/filestorage/FileStorageApplication.java
@@ -0,0 +1,13 @@
+package org.nastya.filestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileStorageApplication.class, args);
+    }
+
+}
